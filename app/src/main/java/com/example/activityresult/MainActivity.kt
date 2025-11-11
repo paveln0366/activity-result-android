f40bd39fbf974707062f03875cf831fd95b292a1
Pavel Potapov <potapov@goozix.com>
2025-11-11 13:51:47 +0300

    Added Activity Result Api

--- app/src/main/java/com/example/activityresult/MainActivity.kt
+++ app/src/main/java/com/example/activityresult/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.activityresult
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -27,32 +27,24 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         initViews()
-        getUserNameButton.setOnClickListener {
-            UserNameActivity.newIntent(this).apply {
-                startActivityForResult(this, RC_USER_NAME)
+
+        val contractUserName = ActivityResultContracts.StartActivityForResult()
+        val launcherUserName = registerForActivityResult(contractUserName) {
+            if (it.resultCode == RESULT_OK) {
+                userNameTextView.text = it.data?.getStringExtra(UserNameActivity.EXTRA_USER_NAME)
             }
+        }
+
+        val contractImage = ActivityResultContracts.GetContent()
+        val launcherImage = registerForActivityResult(contractImage) {
+            imageFromGalleryImageView.setImageURI(it)
+        }
+
+        getUserNameButton.setOnClickListener {
+            launcherUserName.launch(UserNameActivity.newIntent(this))
         }
         getImageButton.setOnClickListener {
-            Intent(Intent.ACTION_PICK).apply {
-                type = "image/*" // MIME types
-                startActivityForResult(this, RC_IMAGE)
-            }
-        }
-    }
-
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_USER_NAME && resultCode == RESULT_OK) {
-            val userName = data?.getStringExtra(UserNameActivity.EXTRA_USER_NAME) ?: ""
-            userNameTextView.text = userName
-        }
-        if (requestCode == RC_IMAGE && resultCode == RESULT_OK) {
-            val uri = data?.data
-            imageFromGalleryImageView.setImageURI(uri)
+            launcherImage.launch("image/*")
         }
     }
 
@@ -61,11 +53,5 @@ class MainActivity : AppCompatActivity() {
         userNameTextView = findViewById(R.id.user_name_textview)
         getImageButton = findViewById(R.id.get_image_button)
         imageFromGalleryImageView = findViewById(R.id.image_from_gallery_imageview)
-    }
-
-    companion object {
-
-        private const val RC_USER_NAME = 100
-        private const val RC_IMAGE = 101
     }
 }
